11109d440370f9ac22abfcec69ec66d80e950d43
zgyzgyhero <894526647@qq.com>
2021-09-26 17:33:13 +0800

    Add export expr fetch api

--- server/models/monitor.go
+++ server/models/monitor.go
@@ -1,79 +1,92 @@
 package models
 
 type MonitorOptionResponse struct {
-	Code  int  `json:"code"`
-	Message  string  `json:"message"`
-	Data  []OptionModel  `json:"data"`
+	Code    int           `json:"code"`
+	Message string        `json:"message"`
+	Data    []OptionModel `json:"data"`
 }
 
 type OptionModel struct {
-	Id  int  `json:"id"`
-	OptionValue  string  `json:"option_value"`
-	OptionText   string  `json:"option_text"`
-	Active  bool  `json:"active"`
-	OptionType  string  `json:"type"`
-	OptionTypeName  string  `json:"option_type_name"`
+	Id             int    `json:"id"`
+	OptionValue    string `json:"option_value"`
+	OptionText     string `json:"option_text"`
+	Active         bool   `json:"active"`
+	OptionType     string `json:"type"`
+	OptionTypeName string `json:"option_type_name"`
 }
 
 type MonitorMetricResponse struct {
-	Code  int  `json:"code"`
-	Message  string  `json:"message"`
-	Data  []MetricOptionModel  `json:"data"`
+	Code    int                 `json:"code"`
+	Message string              `json:"message"`
+	Data    []MetricOptionModel `json:"data"`
 }
 
 type MetricOptionModel struct {
-	Id  int  `json:"id"`
-	Metric  string  `json:"metric"`
-	PromQl  string  `json:"prom_ql"`
+	Id     int    `json:"id"`
+	Metric string `json:"metric"`
+	PromQl string `json:"prom_ql"`
 }
 
 type RespJson struct {
-	Code  int  `json:"code"`
-	Msg   string    `json:"message"`
-	Data  interface{}  `json:"data"`
+	Code int         `json:"code"`
+	Msg  string      `json:"message"`
+	Data interface{} `json:"data"`
 }
 
 type ChartConfigObj struct {
-	Endpoint   string    `form:"endpoint" json:"endpoint"`
-	Metric   string    `form:"metric" json:"metric"`
-	Start  string  `form:"start" json:"start"`
-	End  string  `form:"end" json:"end"`
-	Aggregate  string  `form:"aggregate" json:"agg"`
+	Endpoint  string `form:"endpoint" json:"endpoint"`
+	Metric    string `form:"metric" json:"metric"`
+	Start     string `form:"start" json:"start"`
+	End       string `form:"end" json:"end"`
+	Aggregate string `form:"aggregate" json:"agg"`
 }
 
 type YaxisModel struct {
-	Unit  string  `json:"unit"`
+	Unit string `json:"unit"`
 }
 
 type TimeSerialModel struct {
-	Type  string  `json:"type"`
-	Name  string  `json:"name"`
-	Data  [][]float64  `json:"data"`
+	Type string      `json:"type"`
+	Name string      `json:"name"`
+	Data [][]float64 `json:"data"`
 }
 
 type DataSerialModel struct {
-	Type  string  `json:"type"`
-	Name  string  `json:"name"`
-	Data  []float64  `json:"data"`
+	Type string    `json:"type"`
+	Name string    `json:"name"`
+	Data []float64 `json:"data"`
 }
 
 type AxisModel struct {
-	Data  []float64  `json:"data"`
+	Data []float64 `json:"data"`
 }
 
 type MonitorChartResponse struct {
-	Code  int  `json:"code"`
-	Message  string  `json:"message"`
-	Data  EChartOption  `json:"data"`
+	Code    int          `json:"code"`
+	Message string       `json:"message"`
+	Data    EChartOption `json:"data"`
 }
 
 type EChartOption struct {
-	Id     int     `json:"id"`
-	Title  string  `json:"title"`
-	Legend  []string  `json:"legend"`
-	Xaxis  AxisModel  `json:"xaxis"`
-	Yaxis  YaxisModel  `json:"yaxis"`
-	Series []*TimeSerialModel  `json:"series"`
-	DataSeries  []*DataSerialModel  `json:"data_series"`
-	IsDataSeries  bool  `json:"is_data_series"`
+	Id           int                `json:"id"`
+	Title        string             `json:"title"`
+	Legend       []string           `json:"legend"`
+	Xaxis        AxisModel          `json:"xaxis"`
+	Yaxis        YaxisModel         `json:"yaxis"`
+	Series       []*TimeSerialModel `json:"series"`
+	DataSeries   []*DataSerialModel `json:"data_series"`
+	IsDataSeries bool               `json:"is_data_series"`
+}
+
+type ExportResultParamObj struct {
+	Name     string `json:"name"`
+	Metric   string `json:"metric"`
+	Estimate string `json:"estimate"`
+}
+
+type ExportResultObj struct {
+	Endpoint string                  `json:"endpoint"`
+	XParams  []*ExportResultParamObj `json:"xParams"`
+	YFunc    ExportResultParamObj    `json:"yFunc"`
+	FuncExpr string                  `json:"func_expr"`
 }
--- server/services/monitor.go
+++ server/services/monitor.go
@@ -1,64 +1,141 @@
 package services
 
 import (
-	"github.com/WeBankPartners/wecube-plugins-capacity/server/models"
-	"net/http"
-	"fmt"
 	"bytes"
 	"encoding/json"
+	"fmt"
+	"github.com/WeBankPartners/wecube-plugins-capacity/server/models"
 	"io/ioutil"
+	"net/http"
+	"strings"
 )
 
-func MonitorEndpointSearch(search string) (err error,result []models.OptionModel) {
+func MonitorEndpointSearch(search string) (err error, result []models.OptionModel) {
 	var response models.MonitorOptionResponse
-	err,data := requestMonitor(http.MethodGet, fmt.Sprintf("dashboard/search?page=1&size=1000&search=%s", search), nil)
+	err, data := requestMonitor(http.MethodGet, fmt.Sprintf("dashboard/search?page=1&size=1000&search=%s", search), nil)
 	if err != nil {
 		return err, result
 	}
 	err = json.Unmarshal(data, &response)
-	return err,response.Data
+	return err, response.Data
 }
 
-func MonitorMetricSearch(endpointType string) (err error,result []models.MetricOptionModel)  {
+func MonitorMetricSearch(endpointType string) (err error, result []models.MetricOptionModel) {
 	var response models.MonitorMetricResponse
-	err,data := requestMonitor(http.MethodGet, fmt.Sprintf("dashboard/config/metric/list?type=%s", endpointType), nil)
+	err, data := requestMonitor(http.MethodGet, fmt.Sprintf("dashboard/config/metric/list?type=%s", endpointType), nil)
 	if err != nil {
 		return err, result
 	}
 	err = json.Unmarshal(data, &response)
-	return err,response.Data
+	return err, response.Data
 }
 
-func MonitorChart(param []models.ChartConfigObj) (err error,result models.EChartOption) {
+func MonitorChart(param []models.ChartConfigObj) (err error, result models.EChartOption) {
 	var response models.MonitorChartResponse
-	err,data := requestMonitor(http.MethodPost, "dashboard/newchart", param)
+	err, data := requestMonitor(http.MethodPost, "dashboard/newchart", param)
 	if err != nil {
-		return err,result
+		return err, result
 	}
 	err = json.Unmarshal(data, &response)
-	return err,response.Data
+	return err, response.Data
 }
 
-func requestMonitor(method,url string,postData interface{}) (err error,bodyData []byte) {
+func requestMonitor(method, url string, postData interface{}) (err error, bodyData []byte) {
 	var postBytes []byte
 	if postData != nil {
-		postBytes,err = json.Marshal(postData)
+		postBytes, err = json.Marshal(postData)
 		if err != nil {
-			return err,bodyData
+			return err, bodyData
 		}
 	}
-	request,_ := http.NewRequest(method, fmt.Sprintf("%s/monitor/api/v1/%s", models.Config().DataSource.Monitor.BaseUrl, url), bytes.NewBuffer(postBytes))
+	request, _ := http.NewRequest(method, fmt.Sprintf("%s/monitor/api/v1/%s", models.Config().DataSource.Monitor.BaseUrl, url), bytes.NewBuffer(postBytes))
 	tokenValue := models.Config().DataSource.Monitor.TokenValue
 	if models.Config().DataSource.Monitor.TokenKey == "Authorization" {
 		tokenValue = models.GetCoreToken()
 	}
 	request.Header.Set(models.Config().DataSource.Monitor.TokenKey, tokenValue)
 	request.Header.Set("Content-Type", "application/json")
-	response,err := http.DefaultClient.Do(request)
+	response, err := http.DefaultClient.Do(request)
 	if err != nil {
-		return err,bodyData
+		return err, bodyData
 	}
-	bodyData,err = ioutil.ReadAll(response.Body)
+	bodyData, err = ioutil.ReadAll(response.Body)
 	response.Body.Close()
-	return err,bodyData
+	return err, bodyData
+}
+
+func ExportExprResult(endpoints []string, metric string) (result []*models.ExportResultObj, err error) {
+	result = []*models.ExportResultObj{}
+	specSql, endpointsParam := createListParams(endpoints, "")
+	var rMonitorTables []*models.RMonitorTable
+	err = mysqlEngine.SQL("select distinct guid,endpoint from r_monitor where endpoint in ("+specSql+") order by endpoint", endpointsParam...).Find(&rMonitorTables)
+	if err != nil {
+		err = fmt.Errorf("Try to query rMonitor table fail,%s ", err.Error())
+		return
+	}
+	if len(rMonitorTables) == 0 {
+		return
+	}
+	rMonitorGuidList := []string{}
+	for _, v := range rMonitorTables {
+		rMonitorGuidList = append(rMonitorGuidList, v.Guid)
+	}
+	var rWorkTables []*models.RWorkTable
+	err = mysqlEngine.SQL("select guid,name,expr,func_x,func_x_name,func_b,legend_x,legend_y from r_work where guid in ('" + strings.Join(rMonitorGuidList, "','") + "')").Find(&rWorkTables)
+	if err != nil {
+		err = fmt.Errorf("Try to query rWork table fail,%s ", err.Error())
+		return
+	}
+	rWorkMap := make(map[string]*models.RWorkTable)
+	for _, v := range rWorkTables {
+		rWorkMap[v.Guid] = v
+	}
+	for _, rMonitor := range rMonitorTables {
+		if _, b := rWorkMap[rMonitor.Guid]; !b {
+			continue
+		}
+		tmpLegendY := getLegendMetric(rMonitor.Endpoint, rWorkMap[rMonitor.Guid].LegendY)
+		if metric != "" {
+			if metric != tmpLegendY {
+				continue
+			}
+		}
+		tmpResultObj := models.ExportResultObj{Endpoint: rMonitor.Endpoint, FuncExpr: rWorkMap[rMonitor.Guid].Expr}
+		tmpResultObj.YFunc = models.ExportResultParamObj{Metric: tmpLegendY}
+		tmpResultObj.XParams = buildExportLegendX(rWorkMap[rMonitor.Guid].FuncX, rWorkMap[rMonitor.Guid].FuncXName, rWorkMap[rMonitor.Guid].FuncB, rMonitor.Endpoint)
+		result = append(result, &tmpResultObj)
+		delete(rWorkMap, rMonitor.Guid)
+	}
+	return
+}
+
+func getLegendMetric(endpoint, legend string) string {
+	if strings.HasPrefix(legend, endpoint) {
+		legend = legend[len(endpoint)+1:]
+	}
+	return legend
+}
+
+func buildExportLegendX(estimate, funcX, funcB, endpoint string) []*models.ExportResultParamObj {
+	result := []*models.ExportResultParamObj{}
+	funcXList := strings.Split(funcX, "^")
+	for i, v := range strings.Split(estimate, ",") {
+		tmpObj := models.ExportResultParamObj{Name: fmt.Sprintf("x%d", i+1), Estimate: v}
+		tmpObj.Metric = getLegendMetric(endpoint, funcXList[i])
+		result = append(result, &tmpObj)
+	}
+	result = append(result, &models.ExportResultParamObj{Name: "b", Estimate: funcB})
+	return result
+}
+
+func createListParams(inputList []string, prefix string) (specSql string, paramList []interface{}) {
+	if len(inputList) > 0 {
+		var specList []string
+		for _, v := range inputList {
+			specList = append(specList, "?")
+			paramList = append(paramList, prefix+v)
+		}
+		specSql = strings.Join(specList, ",")
+	}
+	return
 }
